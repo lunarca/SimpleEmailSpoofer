ca41bb6eae2756258310e713c11a6e889e1aa5a3
William DeFreese <alexdefreese@gmail.com>
2015-04-12 23:45:30 -0400

    LOL Spaces

--- libs/PrettyOutput.py
+++ libs/PrettyOutput.py
@@ -2,16 +2,16 @@ from colorama import Fore, Back, Style
 from colorama import init as color_init
 
 def good(line):
-	print Fore.GREEN + Style.BRIGHT + "[+]" + Style.RESET_ALL, line
+    print Fore.GREEN + Style.BRIGHT + "[+]" + Style.RESET_ALL, line
 
 def meh(line):
-	print Fore.BLUE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line
+    print Fore.BLUE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line
 
 def error(line):
-	print Fore.RED + Style.BRIGHT + "[-] !!! " + Style.NORMAL, line, Style.BRIGHT + "!!!"
+    print Fore.RED + Style.BRIGHT + "[-] !!! " + Style.NORMAL, line, Style.BRIGHT + "!!!"
 
 def bad(line):
-	print Fore.RED + Style.BRIGHT + "[-]" + Style.RESET_ALL, line
+    print Fore.RED + Style.BRIGHT + "[-]" + Style.RESET_ALL, line
 
 def info(line):
-	print Fore.WHITE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line
+    print Fore.WHITE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line
